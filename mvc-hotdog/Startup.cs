ce0e8f4f3c1eaf1ec1df4c4b386627d14988812d
Dillion Patrick <dkpat12@gmail.com>
2015-03-23 15:16:46 -0400

    Visual Studio Project created

---
+++ mvc-hotdog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_hotdog.Startup))]
+namespace mvc_hotdog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
