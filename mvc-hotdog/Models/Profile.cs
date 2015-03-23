dd1897cad26fed5032bfc227e78caa58e61d8a84
Dillion Patrick <dkpat12@gmail.com>
2015-03-23 15:27:11 -0400

    Profile class created + Controller

---
+++ mvc-hotdog/Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_hotdog.Models
+{
+    public class Profile
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string ProfileBio { get; set; }
+        public string ImageURL { get; set; }
+        public string FavDog { get; set; }
+        public int Rating { get; set; }
+        public List<string> PastDogs { get; set; }
+    }
+}
